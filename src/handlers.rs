07852bbc0171a7cc348d24dfc902a2e5e24a3e0d
Will Harris <will.t.harris@protonmail.com>
2020-10-25 11:43:07 -0700

    feat: define get_items client handler

--- src/handlers.rs
+++ src/handlers.rs
@@ -22,3 +22,17 @@ pub async fn get_todos(db_pool: web::Data<Pool>) -> impl Responder {
         Err(_) => HttpResponse::InternalServerError().into(),
     }
 }
+
+pub async fn get_items(db_pool: web::Data<Pool>, path: web::Path<(i32,)>) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database");
+
+    let result = db::get_items(&client, path.0).await;
+
+    match result {
+        Ok(items) => HttpResponse::Ok().json(items),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
+}
