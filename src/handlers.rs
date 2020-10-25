0be164d5954a7b7e0c9883dfa9a1cd7a76dc34fc
Will Harris <will.t.harris@protonmail.com>
2020-10-25 11:23:41 -0700

    feat: define get_todos client method

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,8 +1,24 @@
+use crate::db;
 use crate::models::Status;
-use actix_web::{web, Responder};
+use actix_web::{web, HttpResponse, Responder};
+use deadpool_postgres::{Client, Pool};
 
 pub async fn status() -> impl Responder {
     web::HttpResponse::Ok().json(Status {
         status: "UP".to_string(),
     })
+}
+
+pub async fn get_todos(db_pool: web::Data<Pool>) -> impl Responder {
+    let client: Client = db_pool
+        .get()
+        .await
+        .expect("Error connecting to the database");
+
+    let result = db::get_todos(&client).await;
+
+    match result {
+        Ok(todos) => HttpResponse::Ok().json(todos),
+        Err(_) => HttpResponse::InternalServerError().into(),
+    }
 }
