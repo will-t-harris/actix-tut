073245455a79448c0abed1f57c015f3478346ad7
Will Harris <will.t.harris@protonmail.com>
2020-10-25 10:20:05 -0700

    chore: add log to server start

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,14 @@ use actix_web::{web, App, HttpServer, Responder};
 use std::io;
 
 async fn status() -> impl Responder {
-    web::HttpResponse::Ok().json(Status {status: "Ok".to_string()})
+    web::HttpResponse::Ok().json(Status {
+        status: "UP".to_string(),
+    })
 }
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+    println!("Starting server at http://127.0.0.1:8000/");
+
     HttpServer::new(|| App::new().route("/", web::get().to(status)))
         .bind("127.0.0.1:8080")?
         .run()
